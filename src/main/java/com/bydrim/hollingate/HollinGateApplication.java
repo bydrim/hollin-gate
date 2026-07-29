cef542e50148ae4b010019ba760fc9da31c935af
bydrim <22540269+bydrim@users.noreply.github.com>
2026-07-30 00:20:12 +0200

    added logger to the main

--- src/main/java/com/bydrim/hollingate/HollinGateApplication.java
+++ src/main/java/com/bydrim/hollingate/HollinGateApplication.java
@@ -2,6 +2,8 @@ package com.bydrim.hollingate;
 
 
 import com.bydrim.hollingate.configs.GatewayConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -10,13 +12,13 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan({ "com.bydrim.hollingate.configs" })
 public class HollinGateApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HollinGateApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(HollinGateApplication.class);
 		app.addListeners((ApplicationReadyEvent event) -> {
 			GatewayConfig conf = event.getApplicationContext().getBean(GatewayConfig.class);
-			// TODO: use logging instead of console login
-			System.out.println(conf);
+			logger.info("hollin-gate.yaml gateway config:\n{}", conf);
 		});
 		app.run(args);
 	}
